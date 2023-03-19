55503dde33036aec0f3bacb81130e95711d7de50
Vishal-200803 <mourya.vishal2003@gmail.com>
2023-03-19 10:49:19 +0530

    Initail commit

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+
+            # client = pymongo.MongoClient("mongodb+srv://Vishal:Vishal@2003@cluster0.vnfzods.mongodb.net/?retryWrites=true&w=majority")
+            # db = client['review_scrap']
+            # review_col = db['review_scrap_data']
+            # review_col.insert_many(reviews)
